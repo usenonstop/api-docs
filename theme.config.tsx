4619ebb34a9727a7e9cba740141fa9ad1bf83ed5
sinuheshinbr <sinuheshin@gmail.com>
2024-04-25 17:57:35 -0300

    feat: add welcome page

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Logo from "./components/logo";
+import Whatsapp from "./components/whatsapp";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Logo />,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/usenonstop",
+  },
+  editLink: {
+    component: null,
+  },
+  docsRepositoryBase: "https://github.com/usenonstop/api-docs",
+  footer: {
+    text: "nonStop - API",
   },
   chat: {
-    link: 'https://discord.com',
+    link: `https://wa.me/11947461988?text=${encodeURI(
+      "Olá Sinuhe, gostaria de falar sobre as APIs da nonStop",
+    )}`,
+    icon: <Whatsapp />,
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+  nextThemes: { defaultTheme: "dark" },
+  primaryHue: 200,
+};
 
-export default config
+export default config;
